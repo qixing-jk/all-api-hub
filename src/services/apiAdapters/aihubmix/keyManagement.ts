48fba1f55d43c2399f8f29928673f6073a7fd1ec
anime <street-anime-olive@duck.com>
2026-06-19 13:12:56 +0800

    refactor(api-adapters): extend key management lifecycle

--- src/services/apiAdapters/aihubmix/keyManagement.ts
+++ src/services/apiAdapters/aihubmix/keyManagement.ts
@@ -1,7 +1,10 @@
 import type { KeyManagementCapability } from "~/services/apiAdapters/contracts/keyManagement"
 import {
   createApiToken,
+  deleteApiToken,
+  fetchAccountAvailableModels,
   fetchAccountTokens,
+  fetchUserGroups,
   resolveApiTokenKey,
 } from "~/services/apiService/aihubmix"
 
@@ -9,4 +12,7 @@ export const aihubmixKeyManagement: KeyManagementCapability = {
   fetchTokens: (request) => fetchAccountTokens(request),
   createToken: (request, tokenData) => createApiToken(request, tokenData),
   resolveTokenKey: ({ request, token }) => resolveApiTokenKey(request, token),
+  deleteToken: ({ request, tokenId }) => deleteApiToken(request, tokenId),
+  fetchUserGroups: (request) => fetchUserGroups(request),
+  fetchAvailableModels: (request) => fetchAccountAvailableModels(request),
 }
